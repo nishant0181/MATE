004c8faf9ca6865d33bf7a9623d5f0a72e6049e7
nishant0181 <technishant01@gmail.com>
2026-04-14 10:46:48 +0530

    bug fixes

--- vite.config.js
+++ vite.config.js
@@ -3,35 +3,15 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path'
 import { fileURLToPath } from 'url'
-import {VitePWA} from 'vite-plugin-pwa'
+
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(), tailwindcss(),VitePWA({
-    registerType: 'autoUpdate',
-    // includeAssets: ['favicon.svg', 'robots.txt', 'mask-icon.svg'],
-    // manifest: {
-    //   name: 'MATE',
-    //   short_name: 'MATE',
-    //   description: 'MATE',
-    //   theme_color: '#ffffff',
-    //   icons: [
-    //     {
-    //       src: '/pwa-192x192.png',
-    //       sizes: '192x192',
-    //       type: 'image/png',
-    //     },
-    //     {
-    //       src: '/pwa-512x512.png',
-    //       sizes: '512x512',
-    //       type: 'image/png',
-    //     },
-      // ],
-    // },
-  })],
+
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
